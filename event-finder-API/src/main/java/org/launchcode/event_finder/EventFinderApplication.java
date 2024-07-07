758457f60eab8c569316d3a7556f20c0f0ac7b1c
anumohan208 <154403264+anumohan208@users.noreply.github.com>
2024-07-07 18:19:13 -0500

    added event-finder-API template

---
+++ event-finder-API/src/main/java/org/launchcode/event_finder/EventFinderApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.event_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventFinderApplication.class, args);
+	}
+
+}
---
+++ event-finder-API/src/test/java/org/launchcode/event_finder/EventFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.event_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
