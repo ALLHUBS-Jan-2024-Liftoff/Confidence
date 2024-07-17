27a6113b623f76877061caaa59bc18ce957a9c0f
Darren Oday <darren.oday@gmail.com>
2024-07-16 20:50:45 -0500

    test

--- event-finder-API/src/main/java/org/launchcode/event_finder/Models/Event.java
+++ event-finder-API/src/main/java/org/launchcode/event_finder/Models/Event.java
@@ -4,11 +4,11 @@ package org.launchcode.event_finder.Models;
 
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalTime;
 import java.util.Date;
 
