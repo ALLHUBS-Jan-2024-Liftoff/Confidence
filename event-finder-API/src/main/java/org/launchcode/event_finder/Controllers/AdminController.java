e853ef0d6fed86d65663245e26462af4f23cccd4
anumohan208 <154403264+anumohan208@users.noreply.github.com>
2024-07-18 20:06:32 -0500

    Admin dashboard

---
+++ event-finder-API/src/main/java/org/launchcode/event_finder/Controllers/AdminController.java
@@ -0,0 +1,61 @@
+package org.launchcode.event_finder.Controllers;
+import javax.validation.Valid;
+import org.launchcode.event_finder.Repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.launchcode.event_finder.Models.Event;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/api/admin/events")
+public class AdminController {
+
+    private final EventRepository eventRepository;
+
+    @Autowired
+    public AdminController(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    // Admin can Get all user submitted events
+    @GetMapping
+    public List<Event> getAllSubmittedEvents() {
+        return eventRepository.findAll();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Event> updateEvent(@PathVariable Long id,
+                                             @RequestBody @Valid Event event) {
+
+        Optional<Event> eventOptional = eventRepository.findById(id);
+        if (eventOptional.isPresent( )) {
+            Event existingEvent = eventOptional.get( );
+
+            //Update event fields
+            existingEvent.setEventName(event.getEventName( ));
+            existingEvent.setEventDate(event.getEventDate( ));
+            existingEvent.setEventTime(event.getEventTime( ));
+            existingEvent.setEventLocation(event.getEventLocation( ));
+            existingEvent.setDescription(event.getDescription( ));
+            existingEvent.setEventCategory(event.getEventCategory());
+            existingEvent.setEventPrice(event.getEventPrice());
+            existingEvent.setApprovalStatus(event.getApprovalStatus());
+
+            Event updatedEvent = eventRepository.save(existingEvent);
+            return ResponseEntity.ok(updatedEvent);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+}
