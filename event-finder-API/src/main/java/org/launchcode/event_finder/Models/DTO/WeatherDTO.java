3ee6a6f30d7c0f39b0f26821f7159e95b10075ce
i-swa <ievaswanson@u.boisestate.edu>
2024-08-04 17:32:35 -0500

    DTO created... not sure if this is going to work...

--- event-finder-API/src/main/java/org/launchcode/event_finder/Models/DTO/WeatherDTO.java
+++ event-finder-API/src/main/java/org/launchcode/event_finder/Models/DTO/WeatherDTO.java
@@ -1,4 +1,29 @@
 package org.launchcode.event_finder.Models.DTO;
 
 public class WeatherDTO {
+
+    public class WeatherDTO {
+        private String zipLocation;
+        private String zipCurrentTemp;
+
+        // Getters and setters
+        
+        public String getZipLocation() {
+            return zipLocation;
+        }
+
+        public void setZipLocation(String zipLocation) {
+            this.zipLocation = zipLocation;
+        }
+
+        public String getZipCurrentTemp() {
+            return zipCurrentTemp;
+        }
+
+        public void setZipCurrentTemp(String zipCurrentTemp) {
+            this.zipCurrentTemp = zipCurrentTemp;
+        }
+
+    }
+
 }
